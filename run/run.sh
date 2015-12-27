4294b9b864d7e6f5b99ba8aba242a3829731606d
Axel Müller <axel@mueller-family.de>
2015-12-27 19:41:38 +0100

    add command line options

--- run/run.sh
+++ run/run.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-sudo java -Xdebug -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n -Dappliance.dir=/app -jar SmartApplianceEnabler-0.1.0.jar
+JAR=`ls SmartApplianceEnabler-*.jar`
+CONF_DIR=.
+
+usage(){
+  echo "Usage: $0 [OPTION]"
+  echo "  -c <directory>      Directory containing configuration files"
+  echo "  -d                  Enable JVM debugging"
+  exit 1
+}
+
+while getopts ':c:d' OPTION ; do
+  case "$OPTION" in
+    c) CONF_DIR=$OPTARG;;
+    d) DEBUG="-Xdebug -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n";;
+    *) usage
+  esac
+done
+RUN="sudo java $DEBUG -Dappliance.dir=$CONF_DIR -jar $JAR"
+echo $RUN
+$RUN
