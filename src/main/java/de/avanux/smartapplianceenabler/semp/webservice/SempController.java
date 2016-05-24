2a7105df088c8e330cf0ba4951ed1726874379e7
Axel Müller <axel@mueller-family.de>
2016-05-24 20:29:22 +0200

    more logging

--- src/main/java/de/avanux/smartapplianceenabler/semp/webservice/SempController.java
+++ src/main/java/de/avanux/smartapplianceenabler/semp/webservice/SempController.java
@@ -316,6 +316,7 @@ public class SempController {
         timeFrame.setLatestEnd(latestEnd);
         timeFrame.setMinRunningTime(minRunningTime);
         timeFrame.setMaxRunningTime(maxRunningTime);
+        logger.debug("Timeframe added to PlanningRequest: " + timeFrame.toString());
         return timeFrame;
     }
     
--- src/main/java/de/avanux/smartapplianceenabler/semp/webservice/Timeframe.java
+++ src/main/java/de/avanux/smartapplianceenabler/semp/webservice/Timeframe.java
@@ -73,4 +73,12 @@ public class Timeframe {
     public void setMaxRunningTime(Long maxRunningTime) {
         this.maxRunningTime = maxRunningTime;
     }
+
+    @Override
+    public String toString() {
+        return earliestStart
+                + "s-" + latestEnd
+                + "s:" + minRunningTime
+                + "s/" + maxRunningTime + "s";
+    }
 }
