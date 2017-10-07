fdeca5057ea792485fb7f586074f40a7caa1c1ed
Axel Müller <axel@mueller-family.de>
2017-10-07 17:10:40 +0200

    fixed padding/margin; disable fields if schedule is disabled

--- src/main/angular/src/app/schedule/schedule.component.ts
+++ src/main/angular/src/app/schedule/schedule.component.ts
@@ -16,7 +16,10 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-import {AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+import {
+  AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit,
+  Output
+} from '@angular/core';
 import {Schedule} from '../shared/schedule';
 
 declare const $: any;
