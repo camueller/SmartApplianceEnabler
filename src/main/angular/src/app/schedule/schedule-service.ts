fc0245f679f07ed1260df073cb6a1c07eaab64bf
Axel Müller <axel@mueller-family.de>
2017-12-27 14:33:30 +0100

    test for appliance status improved

--- src/main/angular/src/app/schedule/schedule-service.ts
+++ src/main/angular/src/app/schedule/schedule-service.ts
@@ -13,14 +13,14 @@ export class ScheduleService extends SaeService {
   }
 
   getSchedules(id: string): Observable<Array<Schedule>> {
-    return this.http.get(`${this.api}/schedules?id=${id}`)
+    return this.http.get(`${SaeService.API}/schedules?id=${id}`)
       .map((schedules: Array<Schedule>) => {
         return schedules.map(schedule => ScheduleFactory.toSchedule(schedule));
       });
   }
 
   setSchedules(id: string, schedules: Schedule[]): Observable<any> {
-    const url = `${this.api}/schedules?id=${id}`;
+    const url = `${SaeService.API}/schedules?id=${id}`;
     const content = ScheduleFactory.toJSON(schedules);
     console.log('Set schedule using ' + url);
     return this.http.put(url, content, {headers: this.headersContentTypeJson, responseType: 'text'});
