3ce0333c6815e419b7d857a8dae8a57dd287557f
Axel Müller <axel@mueller-family.de>
2018-01-29 19:21:47 +0100

    HolidaysDownloader uses Genson instead of GSON Adapted to new URL

--- src/main/java/de/avanux/smartapplianceenabler/HolidaysDownloader.java
+++ src/main/java/de/avanux/smartapplianceenabler/HolidaysDownloader.java
@@ -17,28 +17,29 @@
  */
 package de.avanux.smartapplianceenabler;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.owlike.genson.Genson;
+import com.owlike.genson.GensonBuilder;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.net.ssl.*;
+import java.io.*;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.text.MessageFormat;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.Scanner;
 
 public class HolidaysDownloader {
     private Logger logger = LoggerFactory.getLogger(HolidaysDownloader.class);
     public static final String urlConfigurationParamName = "Holidays.Url";
-    private String url = "http://feiertage.jarmedia.de/api/?jahr={0}&nur_land=NATIONAL";
+    private String url = "https://feiertage-api.de/api/?jahr={0}&nur_land=NATIONAL";
+
     private static final DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 
     public String getUrl() {
@@ -57,34 +58,50 @@ public class HolidaysDownloader {
 
     public Map<LocalDate, String> downloadHolidays() {
         Map<LocalDate, String> holidayWithName = new LinkedHashMap<>();
-        BufferedReader reader = null;
         try {
-            JsonParser parser = new JsonParser();
+            /**
+             * Avoid
+             *
+             * javax.net.ssl.SSLHandshakeException: sun.security.validator.ValidatorException:
+             * PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:
+             * unable to find valid certification path to requested target
+             *
+             * temporary replacing the trust manager with an all-trusting one
+             */
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager(){
+                public X509Certificate[] getAcceptedIssuers(){return null;}
+                public void checkClientTrusted(X509Certificate[] certs, String authType){}
+                public void checkServerTrusted(X509Certificate[] certs, String authType){}
+            }};
+            // Install SSLSocketFactory with all-trusting trust manager
+            SSLSocketFactory originalSSLSocketFactory = HttpsURLConnection.getDefaultSSLSocketFactory();
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+
             String resolvedUrl = getResolvedUrl();
             logger.debug("Will download holidays from " + resolvedUrl);
-            URL url = new URL(resolvedUrl);
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            JsonObject asJsonArray = parser.parse(reader).getAsJsonObject();
-            final Set<Map.Entry<String, JsonElement>> entries = asJsonArray.entrySet();
-            for(Map.Entry<String, JsonElement> x : entries) {
-                String name = x.getKey();
-                JsonObject datumUndHinweis = x.getValue().getAsJsonObject();
-                String datumString = datumUndHinweis.get("datum").getAsString();
+            Genson genson = new GensonBuilder().useRuntimeType(true).create();
+
+            String out = new Scanner(new URL(resolvedUrl).openStream(), "UTF-8").useDelimiter("\\A").next();
+            logger.debug("Holidays JSON: " + out);
+            Reader reader = new StringReader(out);
+
+            Map<String, Object> json = genson.deserialize(reader, Map.class);
+            for(String holidayName : json.keySet()) {
+                Map<String, Object> dateAndHint = (Map<String, Object>) json.get(holidayName);
+                String datumString = dateAndHint.get("datum").toString();
                 LocalDate datum = dateFormatter.parseLocalDate(datumString);
-                holidayWithName.put(datum, name);
+                holidayWithName.put(datum, holidayName);
             }
+
+            // Restore original SSLSocketFactory
+            HttpsURLConnection.setDefaultSSLSocketFactory(originalSSLSocketFactory);
         }
         catch(Exception e) {
             logger.error("Error downloading holidays", e);
-        }
-        finally {
-            if(reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    logger.error("Error closing reader", e);
-                }
-            }
         }
         logger.debug("Number of holidays: " + holidayWithName.size());
         return holidayWithName;
