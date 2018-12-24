a0e878d48d39f5a5baba94d5e84c89477c6a3a95
Axel M?ller <axel.mueller@avanux.de>
2018-12-24 12:38:16 +0100

    OptionalEnergy is based on MinEnergy=0 (and not MinRunningTime=0).

--- src/main/java/de/avanux/smartapplianceenabler/webservice/SaeController.java
+++ src/main/java/de/avanux/smartapplianceenabler/webservice/SaeController.java
@@ -565,7 +565,8 @@ public class SaeController {
                     applianceStatus.setLatestStart(TimeframeInterval.getLatestStart(nextRuntimeInterval.getLatestEnd(),
                             nextRuntimeInterval.getMinRunningTime()));
                     if(appliance.getControl().isOn()) {
-                        applianceStatus.setOptionalEnergy(nextRuntimeInterval.getMinRunningTime() == 0);
+                        applianceStatus.setOptionalEnergy(nextRuntimeInterval.getMinEnergy() != null
+                                && nextRuntimeInterval.getMinEnergy() == 0);
                     }
                     if(runningTimeMonitor.getActiveTimeframeInterval() == null) {
                         applianceStatus.setRunningTime(0);
