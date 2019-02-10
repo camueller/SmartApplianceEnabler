9416d4e55358e12806b3b12329d9707f04630995
Axel M?ller <axel.mueller@avanux.de>
2019-02-10 12:52:20 +0100

    initial implementation of some tests added

--- src/main/angular/src/app/status-evcharger-view/status-evcharger-view.component.spec.ts
+++ src/main/angular/src/app/status-evcharger-view/status-evcharger-view.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { StatusEvchargerViewComponent } from './status-evcharger-view.component';
+import {StatusEvchargerViewComponent} from './status-evcharger-view.component';
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
+import {FakeTranslateLoader} from '../testing/fake-translate-loader';
+import {Status} from '../status/status';
+import {EvStatus} from '../status/ev-status';
+
+const translations: any = {
+  'dialog.candeactivate': 'Änderungen verwerfen?',
+  'ApplianceComponent.confirmDeletion': 'Wirklich löschen?'
+};
 
 describe('StatusEvchargerViewComponent', () => {
   let component: StatusEvchargerViewComponent;
   let fixture: ComponentFixture<StatusEvchargerViewComponent>;
+  let translate: TranslateService;
+  const defaultStatus = {
+    planningRequested: true,
+    evIdCharging: 1,
+    evStatuses: [
+      {id: 1, name: 'Nissan Leaf'} as EvStatus
+    ]
+  } as Status;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StatusEvchargerViewComponent ]
-    })
-    .compileComponents();
+      declarations: [ StatusEvchargerViewComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useValue: new FakeTranslateLoader(translations)
+          }
+        })
+      ]
+    }).compileComponents();
+
+    translate = TestBed.get(TranslateService);
+    translate.use('de');
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StatusEvchargerViewComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.status = defaultStatus;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
