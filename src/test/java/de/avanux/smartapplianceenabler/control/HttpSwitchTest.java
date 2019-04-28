c38937a7c022a5730c833f1e09f2e09569cbb5fe
Axel Müller <axel.mueller@avanux.de>
2019-04-28 10:25:40 +0200

    XML schema adapted for HttpElectricityMeterType

--- src/test/java/de/avanux/smartapplianceenabler/control/HttpSwitchTest.java
+++ src/test/java/de/avanux/smartapplianceenabler/control/HttpSwitchTest.java
@@ -28,39 +28,39 @@ public class HttpSwitchTest extends TestBase {
 
     private HttpSwitch zwitch = new HttpSwitch();
 
-    @Ignore
-    @Test
-    public void on_EdimaxSP2101W() {
-        zwitch.setOnUrl("http://192.168.69.74:10000/smartplug.cgi");
-        zwitch.setUsername("admin");
-        zwitch.setPassword("12345678");
-        zwitch.setContentType("application/xml");
-        zwitch.setOnData("<?xml version=\"1.0\" encoding=\"UTF8\"?><SMARTPLUG id=\"edimax\"><CMD id=\"setup\"><Device.System.Power.State>ON</Device.System.Power.State></CMD></SMARTPLUG>");
-        zwitch.on(new LocalDateTime(), true);
-    }
-
-    @Ignore
-    @Test
-    public void off_EdimaxSP2101W() {
-        zwitch.setOffUrl("http://192.168.69.74:10000/smartplug.cgi");
-        zwitch.setUsername("admin");
-        zwitch.setPassword("12345678");
-        zwitch.setContentType("application/xml");
-        zwitch.setOffData("<?xml version=\"1.0\" encoding=\"UTF8\"?><SMARTPLUG id=\"edimax\"><CMD id=\"setup\"><Device.System.Power.State>OFF</Device.System.Power.State></CMD></SMARTPLUG>");
-        zwitch.on(new LocalDateTime(),false);
-    }
-
-    @Ignore
-    @Test
-    public void on_SonoffPow() {
-        zwitch.setOnUrl("http://192.168.69.62/cm?cmnd=Power%20On");
-        zwitch.on(new LocalDateTime(),true);
-    }
-
-    @Ignore
-    @Test
-    public void off_SonoffPow() {
-        zwitch.setOffUrl("http://192.168.69.62/cm?cmnd=Power%20Off");
-        zwitch.on(new LocalDateTime(),false);
-    }
+//    @Ignore
+//    @Test
+//    public void on_EdimaxSP2101W() {
+//        zwitch.setOnUrl("http://192.168.69.74:10000/smartplug.cgi");
+//        zwitch.setUsername("admin");
+//        zwitch.setPassword("12345678");
+//        zwitch.setContentType("application/xml");
+//        zwitch.setOnData("<?xml version=\"1.0\" encoding=\"UTF8\"?><SMARTPLUG id=\"edimax\"><CMD id=\"setup\"><Device.System.Power.State>ON</Device.System.Power.State></CMD></SMARTPLUG>");
+//        zwitch.on(new LocalDateTime(), true);
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void off_EdimaxSP2101W() {
+//        zwitch.setOffUrl("http://192.168.69.74:10000/smartplug.cgi");
+//        zwitch.setUsername("admin");
+//        zwitch.setPassword("12345678");
+//        zwitch.setContentType("application/xml");
+//        zwitch.setOffData("<?xml version=\"1.0\" encoding=\"UTF8\"?><SMARTPLUG id=\"edimax\"><CMD id=\"setup\"><Device.System.Power.State>OFF</Device.System.Power.State></CMD></SMARTPLUG>");
+//        zwitch.on(new LocalDateTime(),false);
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void on_SonoffPow() {
+//        zwitch.setOnUrl("http://192.168.69.62/cm?cmnd=Power%20On");
+//        zwitch.on(new LocalDateTime(),true);
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void off_SonoffPow() {
+//        zwitch.setOffUrl("http://192.168.69.62/cm?cmnd=Power%20Off");
+//        zwitch.on(new LocalDateTime(),false);
+//    }
 }
