ff80fdd881af89e85306913003e7734d6d4b625d
Axel Müller <axel.mueller@avanux.de>
2019-05-25 14:45:10 +0200

    fix: don't retrieve SOC if the ElectricVehicleCharger already has it

--- src/main/java/de/avanux/smartapplianceenabler/control/ev/ElectricVehicleCharger.java
+++ src/main/java/de/avanux/smartapplianceenabler/control/ev/ElectricVehicleCharger.java
@@ -359,7 +359,7 @@ public class ElectricVehicleCharger implements Control, ApplianceIdConsumer {
                 if (getConnectedVehicleId() == null) {
                     setConnectedVehicleId(firstVehicle.getId());
                 }
-                if(previousState == State.VEHICLE_NOT_CONNECTED) {
+                if(getConnectedVehicleSoc() == null) {
                     retrieveSoc(firstVehicle);
                 }
             }
