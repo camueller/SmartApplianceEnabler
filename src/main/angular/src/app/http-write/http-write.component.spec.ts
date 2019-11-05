a17bd2b0f2dd2975fcfcb11e52649cb6ded4db66
Axel Müller <axel.mueller@avanux.de>
2019-11-05 20:26:45 +0100

    Joined alle unit tests in one describe

--- src/main/angular/src/app/http-write/http-write.component.spec.ts
+++ src/main/angular/src/app/http-write/http-write.component.spec.ts
@@ -28,158 +28,161 @@ class MockFormGroupDirective {
   form: FormGroup;
 }
 
-describe('HttpWriteComponent Bindings', () => {
-  let component: HttpWriteTestHostComponent;
-  let fixture: ComponentFixture<HttpWriteTestHostComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        HttpWriteComponent,
-        HttpWriteTestHostComponent
-      ],
-      imports: [
-        ReactiveFormsModule,
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useValue: new FakeTranslateLoader(translations)
-          }
-        })
-      ],
-      providers: [
-        Logger,
-        {provide: Options, useValue: {level: Level.DEBUG}},
-        {provide: FormGroupDirective, useValue: {form: new FormGroup({})} as MockFormGroupDirective}
-      ],
-      schemas: [ NO_ERRORS_SCHEMA ]
-    });
-    fixture  = TestBed.createComponent(HttpWriteTestHostComponent);
-    component = fixture.componentInstance;
-
-    const translate = TestBed.get(TranslateService);
-    translate.use('de');
-
-    fixture.detectChanges();
-  }));
-
-  it('should handle "remove" event', () => {
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      // console.log('HTML=', fixture.debugElement.nativeElement.innerHTML);
-      const removeButton = fixture.debugElement.query(By.css('.buttonRemoveHttpWrite'));
-      removeButton.nativeElement.click();
-      expect(component.onHttpWriteRemoveCalled).toBeTruthy();
-    });
-  });
-
-});
-
-
 describe('HttpWriteComponent', () => {
-  let component: HttpWriteComponent;
-  let fixture: ComponentFixture<HttpWriteComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        HttpWriteComponent,
-        HttpWriteValueStubComponent,
-      ],
-      imports: [
-        ReactiveFormsModule,
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useValue: new FakeTranslateLoader(translations)
-          }
-        })
-      ],
-      providers: [
-        Logger,
-        {provide: Options, useValue: {level: Level.DEBUG}},
-        {provide: FormGroupDirective, useValue: {form: new FormGroup({})} as MockFormGroupDirective}
-      ],
-      schemas: [ NO_ERRORS_SCHEMA ]
-    });
-    fixture = TestBed.createComponent(HttpWriteComponent);
-    component = fixture.componentInstance;
+  describe('Bindings', () => {
+    let component: HttpWriteTestHostComponent;
+    let fixture: ComponentFixture<HttpWriteTestHostComponent>;
 
-    const translate = TestBed.get(TranslateService);
-    translate.use('de');
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          HttpWriteComponent,
+          HttpWriteTestHostComponent
+        ],
+        imports: [
+          ReactiveFormsModule,
+          TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useValue: new FakeTranslateLoader(translations)
+            }
+          })
+        ],
+        providers: [
+          Logger,
+          {provide: Options, useValue: {level: Level.DEBUG}},
+          {provide: FormGroupDirective, useValue: {form: new FormGroup({})} as MockFormGroupDirective}
+        ],
+        schemas: [ NO_ERRORS_SCHEMA ]
+      });
+      fixture  = TestBed.createComponent(HttpWriteTestHostComponent);
+      component = fixture.componentInstance;
 
-    component.formHandler.markLabelsRequired = jest.fn();
-    component.translationKeys = ['ControlHttpComponent.On', 'ControlHttpComponent.Off'];
-    component.formControlNamePrefix = 'write';
+      const translate = TestBed.get(TranslateService);
+      translate.use('de');
 
-    fixture.detectChanges();
-
-    component.httpWrite.writeValues[0].name = ControlValueName.On;
-    component.translationPrefix = 'ControlHttpComponent.';
-
-    // fixture.detectChanges();
-    // fixture.whenStable().then(() => {
-    //   console.log('HTML=', fixture.debugElement.nativeElement.innerHTML);
-    // });
-  }));
-
-  describe('Initially', () => {
-
-    it('a HttpWrite exists', () => {
-      expect(component.httpWrite).toBeTruthy();
-    });
-
-    it('the HttpWrite contains one HttpWriteValue', async( () => {
-      expect(component.httpWrite.writeValues.length).toBe(1);
+      fixture.detectChanges();
     }));
 
-  });
-
-  describe('URL field', () => {
-    let url: DebugElement;
-    let urlNE: any;
-
-    beforeEach(() => {
-      url = fixture.debugElement.query(By.css('input[ng-reflect-name="writeUrl"]'));
-      urlNE = url.nativeElement;
-    });
-
-    it('exists', () => {
-      expect(url).toBeTruthy();
-    });
-
-    // FIXME: find from input via parent
-    it('has label', () => {
-      expect(fixture.debugElement.query(By.css('label'))).toBeTruthy();
-    });
-
-    it('with valid URL should make the form valid', () => {
-      const inputValue = 'http://web.de';
-      urlNE.value = inputValue;
-      urlNE.dispatchEvent(new Event('input'));
-      expect(component.form.controls['writeUrl'].value).toBe(inputValue);
-      expect(component.form.valid).toBeTruthy();
-    });
-
-    it('with invalid URL should make the form invalid', () => {
-      const inputValue = 'http:web.de';
-      urlNE.value = inputValue;
-      urlNE.dispatchEvent(new Event('input'));
-      expect(component.form.controls['writeUrl'].value).toBe(inputValue);
-      expect(component.form.valid).toBeFalsy();
-    });
-
-    it('with invalid URL should display an error message', () => {
-      const inputValue = 'http:web.de';
-      urlNE.value = inputValue;
-      urlNE.dispatchEvent(new Event('input'));
+    it('should handle "remove" event', () => {
       fixture.detectChanges();
       fixture.whenStable().then(() => {
-        const error = fixture.debugElement.query(By.css('div.negative'));
-        expect(error.nativeElement.innerHTML).toBe('Die URL muss gültig sein');
+        // console.log('HTML=', fixture.debugElement.nativeElement.innerHTML);
+        const removeButton = fixture.debugElement.query(By.css('.buttonRemoveHttpWrite'));
+        removeButton.nativeElement.click();
+        expect(component.onHttpWriteRemoveCalled).toBeTruthy();
       });
     });
+
   });
 
+
+  describe('Component', () => {
+    let component: HttpWriteComponent;
+    let fixture: ComponentFixture<HttpWriteComponent>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          HttpWriteComponent,
+          HttpWriteValueStubComponent,
+        ],
+        imports: [
+          ReactiveFormsModule,
+          TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useValue: new FakeTranslateLoader(translations)
+            }
+          })
+        ],
+        providers: [
+          Logger,
+          {provide: Options, useValue: {level: Level.DEBUG}},
+          {provide: FormGroupDirective, useValue: {form: new FormGroup({})} as MockFormGroupDirective}
+        ],
+        schemas: [ NO_ERRORS_SCHEMA ]
+      });
+      fixture = TestBed.createComponent(HttpWriteComponent);
+      component = fixture.componentInstance;
+
+      const translate = TestBed.get(TranslateService);
+      translate.use('de');
+
+      component.formHandler.markLabelsRequired = jest.fn();
+      component.translationKeys = ['ControlHttpComponent.On', 'ControlHttpComponent.Off'];
+      component.formControlNamePrefix = 'write';
+
+      fixture.detectChanges();
+
+      component.httpWrite.writeValues[0].name = ControlValueName.On;
+      component.translationPrefix = 'ControlHttpComponent.';
+
+      // fixture.detectChanges();
+      // fixture.whenStable().then(() => {
+      //   console.log('HTML=', fixture.debugElement.nativeElement.innerHTML);
+      // });
+    }));
+
+    describe('Initially', () => {
+
+      it('a HttpWrite exists', () => {
+        expect(component.httpWrite).toBeTruthy();
+      });
+
+      it('the HttpWrite contains one HttpWriteValue', async( () => {
+        expect(component.httpWrite.writeValues.length).toBe(1);
+      }));
+
+    });
+
+    describe('URL field', () => {
+      let url: DebugElement;
+      let urlNE: any;
+
+      beforeEach(() => {
+        url = fixture.debugElement.query(By.css('input[ng-reflect-name="writeUrl"]'));
+        urlNE = url.nativeElement;
+      });
+
+      it('exists', () => {
+        expect(url).toBeTruthy();
+      });
+
+      // FIXME: find from input via parent
+      it('has label', () => {
+        expect(fixture.debugElement.query(By.css('label'))).toBeTruthy();
+      });
+
+      it('with valid URL should make the form valid', () => {
+        const inputValue = 'http://web.de';
+        urlNE.value = inputValue;
+        urlNE.dispatchEvent(new Event('input'));
+        expect(component.form.controls['writeUrl'].value).toBe(inputValue);
+        expect(component.form.valid).toBeTruthy();
+      });
+
+      it('with invalid URL should make the form invalid', () => {
+        const inputValue = 'http:web.de';
+        urlNE.value = inputValue;
+        urlNE.dispatchEvent(new Event('input'));
+        expect(component.form.controls['writeUrl'].value).toBe(inputValue);
+        expect(component.form.valid).toBeFalsy();
+      });
+
+      it('with invalid URL should display an error message', () => {
+        const inputValue = 'http:web.de';
+        urlNE.value = inputValue;
+        urlNE.dispatchEvent(new Event('input'));
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+          const error = fixture.debugElement.query(By.css('div.negative'));
+          expect(error.nativeElement.innerHTML).toBe('Die URL muss gültig sein');
+        });
+      });
+    });
+
+  });
 
 });
