63764044ac53e8531acc2af479deae7d9acec568
Axel Müller <axel.mueller@avanux.de>
2019-12-21 13:15:55 +0100

    SaeControllerTest fixed - setting xmlns is required now in order to set schedules!

--- src/main/java/de/avanux/smartapplianceenabler/schedule/Schedules.java
+++ src/main/java/de/avanux/smartapplianceenabler/schedule/Schedules.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Axel Müller <axel.mueller@avanux.de>
+ * Copyright (C) 2019 Axel Müller <axel.mueller@avanux.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,9 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-package de.avanux.smartapplianceenabler.webservice;
-
-import de.avanux.smartapplianceenabler.schedule.Schedule;
+package de.avanux.smartapplianceenabler.schedule;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
--- src/test/java/de/avanux/smartapplianceenabler/webservice/SaeControllerTest.java
+++ src/test/java/de/avanux/smartapplianceenabler/webservice/SaeControllerTest.java
@@ -26,155 +26,156 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-//@AutoConfigureMockMvc
-//@ContextConfiguration(classes = {SaeController.class})
-//@WebMvcTest
-//public class SaeControllerTest {
-//
-//    private final static String SCHEDULE_RUNTIME_REQUEST_DAY_TIMEFRAME =
-//            "<Schedules>\n" +
-//            "  <Schedule>\n" +
-//            "    <RuntimeRequest min=\"7200\" max=\"10800\" />\n" +
-//            "    <DayTimeframe>\n" +
-//            "      <Start hour=\"10\" minute=\"0\" second=\"0\"/>\n" +
-//            "      <End hour=\"14\" minute=\"0\" second=\"0\"/>\n" +
-//            "      <DayOfWeek>6</DayOfWeek>\n" +
-//            "      <DayOfWeek>7</DayOfWeek>\n" +
-//            "    </DayTimeframe>\n" +
-//            "  </Schedule>\n" +
-//            "</Schedules>\n"
-//            ;
-//
-//    private  final static String SCHEDULE_CONSECUTIVE_DAYS_TIMEFRAME =
-//            "<Schedules>\n" +
-//            "  <Schedule>\n" +
-//            "    <RuntimeRequest min=\"36000\" max=\"43200\" />\n" +
-//            "    <ConsecutiveDaysTimeframe>\n" +
-//            "      <Start dayOfWeek=\"5\" hour=\"16\" minute=\"0\" second=\"0\" />\n" +
-//            "      <End dayOfWeek=\"7\" hour=\"20\" minute=\"0\" second=\"0\" />\n" +
-//            "    </ConsecutiveDaysTimeframe>\n" +
-//            "  </Schedule>\n" +
-//            "</Schedules>\n"
-//            ;
-//
-//    private final static String SCHEDULE_ENERGY_REQUEST_DAY_TIMEFRAME =
-//            "<Schedules>\n" +
-//                    "  <Schedule>\n" +
-//                    "    <EnergyRequest min=\"1380\" max=\"7360\" />\n" +
-//                    "    <DayTimeframe>\n" +
-//                    "      <Start hour=\"10\" minute=\"0\" second=\"0\"/>\n" +
-//                    "      <End hour=\"14\" minute=\"0\" second=\"0\"/>\n" +
-//                    "      <DayOfWeek>6</DayOfWeek>\n" +
-//                    "      <DayOfWeek>7</DayOfWeek>\n" +
-//                    "    </DayTimeframe>\n" +
-//                    "  </Schedule>\n" +
-//                    "</Schedules>\n"
-//            ;
-//
-//    private MediaType contentType = new MediaType(MediaType.APPLICATION_XML.getType(),
-//            MediaType.APPLICATION_XML.getSubtype(),
-//            Charset.forName("utf8"));
-//
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private WebApplicationContext webApplicationContext;
-//
-//    @BeforeEach
-//    public void setup() throws Exception {
-//        System.setProperty(FileHandler.SAE_HOME, System.getProperty("java.io.tmpdir"));
-//        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-//    }
-//
-//    @Test
-//    public void setSchedules_RuntimeRequest_DayTimeframe() throws Exception {
-//        RunningTimeMonitor runningTimeMonitor = runTest(SaeController.SCHEDULES_URL, SCHEDULE_RUNTIME_REQUEST_DAY_TIMEFRAME);
-//
-//        List<Schedule> schedules = runningTimeMonitor.getSchedules();
-//        assertEquals(1, schedules.size());
-//        Schedule schedule = schedules.get(0);
-//
-//        assertEquals(7200, schedule.getRequest().getMin().intValue());
-//        assertEquals(10800, schedule.getRequest().getMax().intValue());
-//
-//        Timeframe timeframe = schedule.getTimeframe();
-//        assertTrue(timeframe instanceof DayTimeframe);
-//        DayTimeframe dayTimeframe = (DayTimeframe) timeframe;
-//
-//        assertTrue(schedule.getRequest() instanceof RuntimeRequest);
-//        assertEquals(new TimeOfDay(10, 0 ,0), dayTimeframe.getStart());
-//        assertEquals(new TimeOfDay(14, 0 ,0), dayTimeframe.getEnd());
-//
-//        List<Integer> daysOfWeekValues = dayTimeframe.getDaysOfWeekValues();
-//        assertEquals(2, daysOfWeekValues.size());
-//        assertEquals(6, daysOfWeekValues.get(0).intValue());
-//        assertEquals(7, daysOfWeekValues.get(1).intValue());
-//    }
-//
-////    @Test
-////    public void setSchedules_RuntimeRequest_ConsecutiveDaysTimeframe() throws Exception {
-////        RunningTimeMonitor runningTimeMonitor = runTest(SaeController.SCHEDULES_URL, SCHEDULE_CONSECUTIVE_DAYS_TIMEFRAME);
-////
-////        List<Schedule> schedules = runningTimeMonitor.getSchedules();
-////        Assert.assertEquals(1, schedules.size());
-////        Schedule schedule = schedules.get(0);
-////
-////        Assert.assertTrue(schedule.getRequest() instanceof RuntimeRequest);
-////        Assert.assertEquals(36000, schedule.getRequest().getMin().intValue());
-////        Assert.assertEquals(43200, schedule.getRequest().getMax().intValue());
-////
-////        Timeframe timeframe = schedule.getTimeframe();
-////        Assert.assertTrue(timeframe instanceof ConsecutiveDaysTimeframe);
-////
-////        ConsecutiveDaysTimeframe consecutiveDaysTimeframe = (ConsecutiveDaysTimeframe) timeframe;
-////        Assert.assertEquals(new TimeOfDayOfWeek(5, 16, 0, 0), consecutiveDaysTimeframe.getStart());
-////        Assert.assertEquals(new TimeOfDayOfWeek(7, 20, 0, 0), consecutiveDaysTimeframe.getEnd());
-////    }
-////
-////    @Test
-////    public void setSchedules_EnergyRequest_DayTimeframe() throws Exception {
-////        RunningTimeMonitor runningTimeMonitor = runTest(SaeController.SCHEDULES_URL, SCHEDULE_ENERGY_REQUEST_DAY_TIMEFRAME);
-////
-////        List<Schedule> schedules = runningTimeMonitor.getSchedules();
-////        Assert.assertEquals(1, schedules.size());
-////        Schedule schedule = schedules.get(0);
-////
-////        Assert.assertTrue(schedule.getRequest() instanceof EnergyRequest);
-////        Assert.assertEquals(1380, schedule.getRequest().getMin().intValue());
-////        Assert.assertEquals(7360, schedule.getRequest().getMax().intValue());
-////
-////        Timeframe timeframe = schedule.getTimeframe();
-////        Assert.assertTrue(timeframe instanceof DayTimeframe);
-////        DayTimeframe dayTimeframe = (DayTimeframe) timeframe;
-////
-////        Assert.assertEquals(new TimeOfDay(10, 0 ,0), dayTimeframe.getStart());
-////        Assert.assertEquals(new TimeOfDay(14, 0 ,0), dayTimeframe.getEnd());
-////
-////        List<Integer> daysOfWeekValues = dayTimeframe.getDaysOfWeekValues();
-////        Assert.assertEquals(2, daysOfWeekValues.size());
-////        Assert.assertEquals(6, daysOfWeekValues.get(0).intValue());
-////        Assert.assertEquals(7, daysOfWeekValues.get(1).intValue());
-////    }
-//
-//    private RunningTimeMonitor runTest(String url, String content) throws Exception {
-//        RunningTimeMonitor runningTimeMonitor = new RunningTimeMonitor();
-//
-//        String applianceId = "F-00000001-000000000001-00";
-//        Appliance appliance = new Appliance();
-//        appliance.setId(applianceId);
-//        appliance.setRunningTimeMonitor(runningTimeMonitor);
-//
-//        Appliances appliances = new Appliances();
-//        appliances.setAppliances(Collections.singletonList(appliance));
-//
-//        ApplianceManager.getInstanceWithoutTimer().setAppliances(appliances);
-//
-//        this.mockMvc.perform(post(url)
-//                .param("id", applianceId)
-//                .contentType(contentType)
-//                .content(content))
-//                .andExpect(status().isOk());
-//
-//        return runningTimeMonitor;
-//    }
-//}
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {SaeController.class})
+@WebMvcTest
+public class SaeControllerTest {
+
+    private final static String SCHEDULE_RUNTIME_REQUEST_DAY_TIMEFRAME =
+            "<Schedules xmlns=\"http://github.com/camueller/SmartApplianceEnabler/v1.4\">\n" +
+            "  <Schedule>\n" +
+            "    <RuntimeRequest min=\"7200\" max=\"10800\" />\n" +
+            "    <DayTimeframe>\n" +
+            "      <Start hour=\"10\" minute=\"0\" second=\"0\"/>\n" +
+            "      <End hour=\"14\" minute=\"0\" second=\"0\"/>\n" +
+            "      <DayOfWeek>6</DayOfWeek>\n" +
+            "      <DayOfWeek>7</DayOfWeek>\n" +
+            "    </DayTimeframe>\n" +
+            "  </Schedule>\n" +
+            "</Schedules>\n"
+            ;
+
+    private  final static String SCHEDULE_CONSECUTIVE_DAYS_TIMEFRAME =
+            "<Schedules xmlns=\"http://github.com/camueller/SmartApplianceEnabler/v1.4\">\n" +
+            "  <Schedule>\n" +
+            "    <RuntimeRequest min=\"36000\" max=\"43200\" />\n" +
+            "    <ConsecutiveDaysTimeframe>\n" +
+            "      <Start dayOfWeek=\"5\" hour=\"16\" minute=\"0\" second=\"0\" />\n" +
+            "      <End dayOfWeek=\"7\" hour=\"20\" minute=\"0\" second=\"0\" />\n" +
+            "    </ConsecutiveDaysTimeframe>\n" +
+            "  </Schedule>\n" +
+            "</Schedules>\n"
+            ;
+
+    private final static String SCHEDULE_ENERGY_REQUEST_DAY_TIMEFRAME =
+            "<Schedules xmlns=\"http://github.com/camueller/SmartApplianceEnabler/v1.4\">\n" +
+                    "  <Schedule>\n" +
+                    "    <EnergyRequest min=\"1380\" max=\"7360\" />\n" +
+                    "    <DayTimeframe>\n" +
+                    "      <Start hour=\"10\" minute=\"0\" second=\"0\"/>\n" +
+                    "      <End hour=\"14\" minute=\"0\" second=\"0\"/>\n" +
+                    "      <DayOfWeek>6</DayOfWeek>\n" +
+                    "      <DayOfWeek>7</DayOfWeek>\n" +
+                    "    </DayTimeframe>\n" +
+                    "  </Schedule>\n" +
+                    "</Schedules>\n"
+            ;
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_XML.getType(),
+            MediaType.APPLICATION_XML.getSubtype(),
+            Charset.forName("utf8"));
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        System.setProperty(FileHandler.SAE_HOME, System.getProperty("java.io.tmpdir"));
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void setSchedules_RuntimeRequest_DayTimeframe() throws Exception {
+        RunningTimeMonitor runningTimeMonitor = runTest(SaeController.SCHEDULES_URL, SCHEDULE_RUNTIME_REQUEST_DAY_TIMEFRAME);
+
+        List<Schedule> schedules = runningTimeMonitor.getSchedules();
+        assertEquals(1, schedules.size());
+        Schedule schedule = schedules.get(0);
+
+        assertEquals(7200, schedule.getRequest().getMin().intValue());
+        assertEquals(10800, schedule.getRequest().getMax().intValue());
+
+        Timeframe timeframe = schedule.getTimeframe();
+        assertTrue(timeframe instanceof DayTimeframe);
+        DayTimeframe dayTimeframe = (DayTimeframe) timeframe;
+
+        assertTrue(schedule.getRequest() instanceof RuntimeRequest);
+        assertEquals(new TimeOfDay(10, 0 ,0), dayTimeframe.getStart());
+        assertEquals(new TimeOfDay(14, 0 ,0), dayTimeframe.getEnd());
+
+        List<Integer> daysOfWeekValues = dayTimeframe.getDaysOfWeekValues();
+        assertEquals(2, daysOfWeekValues.size());
+        assertEquals(6, daysOfWeekValues.get(0).intValue());
+        assertEquals(7, daysOfWeekValues.get(1).intValue());
+    }
+
+    @Test
+    public void setSchedules_RuntimeRequest_ConsecutiveDaysTimeframe() throws Exception {
+        RunningTimeMonitor runningTimeMonitor = runTest(SaeController.SCHEDULES_URL, SCHEDULE_CONSECUTIVE_DAYS_TIMEFRAME);
+
+        List<Schedule> schedules = runningTimeMonitor.getSchedules();
+        assertEquals(1, schedules.size());
+        Schedule schedule = schedules.get(0);
+
+        assertTrue(schedule.getRequest() instanceof RuntimeRequest);
+        assertEquals(36000, schedule.getRequest().getMin().intValue());
+        assertEquals(43200, schedule.getRequest().getMax().intValue());
+
+        Timeframe timeframe = schedule.getTimeframe();
+        assertTrue(timeframe instanceof ConsecutiveDaysTimeframe);
+
+        ConsecutiveDaysTimeframe consecutiveDaysTimeframe = (ConsecutiveDaysTimeframe) timeframe;
+        assertEquals(new TimeOfDayOfWeek(5, 16, 0, 0), consecutiveDaysTimeframe.getStart());
+        assertEquals(new TimeOfDayOfWeek(7, 20, 0, 0), consecutiveDaysTimeframe.getEnd());
+    }
+
+    @Test
+    public void setSchedules_EnergyRequest_DayTimeframe() throws Exception {
+        RunningTimeMonitor runningTimeMonitor = runTest(SaeController.SCHEDULES_URL, SCHEDULE_ENERGY_REQUEST_DAY_TIMEFRAME);
+
+        List<Schedule> schedules = runningTimeMonitor.getSchedules();
+        assertEquals(1, schedules.size());
+        Schedule schedule = schedules.get(0);
+
+        assertTrue(schedule.getRequest() instanceof EnergyRequest);
+        assertEquals(1380, schedule.getRequest().getMin().intValue());
+        assertEquals(7360, schedule.getRequest().getMax().intValue());
+
+        Timeframe timeframe = schedule.getTimeframe();
+        assertTrue(timeframe instanceof DayTimeframe);
+        DayTimeframe dayTimeframe = (DayTimeframe) timeframe;
+
+        assertEquals(new TimeOfDay(10, 0 ,0), dayTimeframe.getStart());
+        assertEquals(new TimeOfDay(14, 0 ,0), dayTimeframe.getEnd());
+
+        List<Integer> daysOfWeekValues = dayTimeframe.getDaysOfWeekValues();
+        assertEquals(2, daysOfWeekValues.size());
+        assertEquals(6, daysOfWeekValues.get(0).intValue());
+        assertEquals(7, daysOfWeekValues.get(1).intValue());
+    }
+
+    private RunningTimeMonitor runTest(String url, String content) throws Exception {
+        RunningTimeMonitor runningTimeMonitor = new RunningTimeMonitor();
+
+        String applianceId = "F-00000001-000000000001-00";
+        Appliance appliance = new Appliance();
+        appliance.setId(applianceId);
+        appliance.setRunningTimeMonitor(runningTimeMonitor);
+
+        Appliances appliances = new Appliances();
+        appliances.setAppliances(Collections.singletonList(appliance));
+
+        ApplianceManager.getInstanceWithoutTimer().setAppliances(appliances);
+
+        this.mockMvc.perform(post(url)
+                .param("id", applianceId)
+                .contentType(contentType)
+                .content(content))
+                .andExpect(status().isOk());
+
+        return runningTimeMonitor;
+    }
+}
