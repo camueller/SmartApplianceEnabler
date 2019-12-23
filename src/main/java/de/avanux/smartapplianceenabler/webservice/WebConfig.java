7de8cd546af8eb8d8f87ab61ebb5e174357a0942
Axel Müller <axel.mueller@avanux.de>
2019-12-23 17:30:34 +0100

    default view controller redirects to index.html

--- src/main/java/de/avanux/smartapplianceenabler/webservice/WebConfig.java
+++ src/main/java/de/avanux/smartapplianceenabler/webservice/WebConfig.java
@@ -22,18 +22,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.io.IOException;
 import java.util.List;
 
 @EnableWebMvc
+@ComponentScan(basePackages = "de.avanux.smartapplianceenabler")
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -41,6 +46,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private HttpMessageConverters messageConverters;
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/static/index.html");
+    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
