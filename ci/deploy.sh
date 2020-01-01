ba6c401f3ebcf7931db771ea14574833798d58e2
Axel Müller <axel.mueller@avanux.de>
2020-01-01 15:41:01 +0100

    Configure CI

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-sudo docker pull avanux/smartapplianceenabler-amd64
+REPO_TAG="avanux/smartapplianceenabler-amd64:ci"
+CONTAINER_NAME="sae"
+SAE_VOLUME="sae"
+SAE_PORT=80
+
+sudo docker pull $REPO_TAG
+sudo docker stop $CONTAINER_NAME
+sudo docker volume rm -f $SAE_VOLUME
+sudo docker volume create $SAE_VOLUME
+sudo docker run -d --rm -v $SAE_VOLUME:/opt/sae/data -p$SAE_PORT:8080 --name=$CONTAINER_NAME $REPO_TAG
