1cdae9062ec01d43b74a37d4f8fbafb1d6d3fe0d
Axel Müller <axel.mueller@avanux.de>
2020-01-03 13:11:13 +0100

    Using generated applianceId in order to allow parallel browser testing

--- src/test/angular/src/shared/appliance-id-generator.ts
+++ src/test/angular/src/shared/appliance-id-generator.ts
@@ -3,5 +3,5 @@
  */
 export function generateApplianceId(): string {
   const id = Math.floor(Math.random() * 999999999999) + 1;
-  return `F-00000001-${id.toString().padStart(12)}-00`;
+  return `F-00000001-${id.toString().padStart(12, '0')}-00`;
 }
