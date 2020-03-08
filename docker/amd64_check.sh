8cc8ce3e1de7d1e5729f9da4a2b5ad0bf576add8
Axel Müller <axel.mueller@avanux.de>
2020-03-08 20:44:38 +0100

    Copy war file for image build since it is not yet available as download at that time

--- docker/amd64_check.sh
+++ docker/amd64_check.sh
@@ -8,5 +8,6 @@ curl -o $TAGS_FILE https://hub.docker.com/v2/repositories/$NAME/tags
 
 if grep -q -v "name\":\"$SAE_VERSION" $TAGS_FILE; then
   echo "No image for version $SAE_VERSION found on dockerhub."
-   ./build_image.sh $NAME $SAE_VERSION ./sae-amd64
+  cp ../target/SmartApplianceEnabler-$SAE_VERSION.war sae-amd64/
+  ./build_image.sh $NAME $SAE_VERSION ./sae-amd64
 fi
