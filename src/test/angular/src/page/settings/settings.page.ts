0db8bd36b232ad4ee26aa055a7bb4361c33d13c7
Axel Müller <axel.mueller@avanux.de>
2020-04-19 17:41:48 +0200

    #99 handle multiple modbus settings

--- src/test/angular/src/page/settings/settings.page.ts
+++ src/test/angular/src/page/settings/settings.page.ts
@@ -3,17 +3,22 @@ import {ModbusSetting} from '../../../../../main/angular/src/app/settings/modbus
 import {TopMenu} from '../top-menu.page';
 import {ModbusPage} from './modbus.page';
 import {SideMenu} from '../side.menu.page';
+import {clickButton} from '../../shared/form';
 
 export class SettingsPage {
+  private static selectorBase = 'app-settings';
   private static saveButton = Selector('button[type="submit"]');
+  private static addModbusButton = Selector(`${SettingsPage.selectorBase} button.SettingsComponent__addModbusSetting`);
 
   public static async addModbus(t: TestController, modbusSettings: ModbusSetting) {
     await TopMenu.clickMenu(t);
     await SideMenu.clickSettings(t);
-    await ModbusPage.addModbus(t, modbusSettings);
-    await t.click(SettingsPage.saveButton);
+    const modbusSettingsIndex = await ModbusPage.getModbusSettingsCount();
+    await clickButton(t, SettingsPage.addModbusButton);
+    await ModbusPage.addModbus(t, modbusSettings, modbusSettingsIndex);
+    await clickButton(t, SettingsPage.saveButton);
     await SideMenu.clickStatus(t);
     await SideMenu.clickSettings(t);
-    await ModbusPage.assertModbus(t, modbusSettings);
+    await ModbusPage.assertModbus(t, modbusSettings, modbusSettingsIndex);
   }
 }
