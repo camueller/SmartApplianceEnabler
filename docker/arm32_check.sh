88d89487ce4016a99ac4a8238ce4555f3378a355
Axel Müller <axel.mueller@avanux.de>
2020-05-01 10:33:41 +0200

    using same procedure as for amd64 images

--- docker/arm32_check.sh
+++ docker/arm32_check.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-IMAGE_RELEASE_FILE=image.release
-RELEASES_FILE=/tmp/releases
-curl -o $RELEASES_FILE https://api.github.com/repos/camueller/SmartApplianceEnabler/releases
-LATEST_RELEASE=`grep tag_name $RELEASES_FILE | awk -F ':' '{print $2}' | awk -F '"' '{print $2}'i | head -n 1`
-IMAGE_RELEASE=`cat $IMAGE_RELEASE_FILE`
-if [ "$LATEST_RELEASE" != "$IMAGE_RELEASE" ]; then
-  echo "New release detected: $LATEST_RELEASE"
-  ./build_image.sh avanux/smartapplianceenabler-arm32 $LATEST_RELEASE ./sae-arm32
-  echo $LATEST_RELEASE > $IMAGE_RELEASE_FILE
-else
-  echo "Release not new: $LATEST_RELEASE"
-fi
+NAME="avanux/smartapplianceenabler-arm32"
+TAGS_FILE=arm32.tags
+SAE_VERSION=`grep 'ENV SAE_VERSION' sae-arm32/Dockerfile | awk -F '=' '{print $2}'`
+echo "Version from Dockerfile: $SAE_VERSION"
 
+curl -o $TAGS_FILE https://hub.docker.com/v2/repositories/$NAME/tags
+
+if grep -q -v "name\":\"$SAE_VERSION" $TAGS_FILE; then
+  echo "No image for version $SAE_VERSION found on dockerhub."
+  cp ../target/SmartApplianceEnabler-$SAE_VERSION.war sae-amd64/
+  ./build_image.sh avanux/smartapplianceenabler-arm32 $LATEST_RELEASE ./sae-arm32
+fi
