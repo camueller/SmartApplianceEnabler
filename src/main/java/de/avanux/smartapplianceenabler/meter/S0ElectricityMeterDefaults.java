145de97561832fb80f157e4be28578822fc7d439
Axel Müller <axel.mueller@avanux.de>
2021-03-17 15:45:07 +0100

    minPulseDuration decrased to 15 from 20 in order to avoid ignoring some (obviously shorter) pulses by some power meters

--- src/main/java/de/avanux/smartapplianceenabler/meter/S0ElectricityMeterDefaults.java
+++ src/main/java/de/avanux/smartapplianceenabler/meter/S0ElectricityMeterDefaults.java
@@ -20,7 +20,7 @@ package de.avanux.smartapplianceenabler.meter;
 
 public class S0ElectricityMeterDefaults {
     // static members won't be serialized but we need those valus on the client
-    private Integer minPulseDuration = 20;
+    private Integer minPulseDuration = 15;
     private static S0ElectricityMeterDefaults instance = new S0ElectricityMeterDefaults();
 
     public static Integer getMinPulseDuration() {
