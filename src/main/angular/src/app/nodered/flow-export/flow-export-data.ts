566eb4575c36400e946a84d31249dad055bbe101
Axel Müller <axel.mueller@avanux.de>
2022-01-03 16:38:07 +0100

    use MQTT configuration for Node-RED JSON generation

--- src/main/angular/src/app/nodered/flow-export/flow-export-data.ts
+++ src/main/angular/src/app/nodered/flow-export/flow-export-data.ts
@@ -16,6 +16,9 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+import {MqttSettings} from '../../settings/mqtt-settings';
+
 export class FlowExportData {
   applianceIds: string[];
+  mqttSettings: MqttSettings;
 }
