75e5a3dcccf94044152a04ea032b88dfda49d327
Axel Müller <axel.mueller@avanux.de>
2024-03-17 06:32:50 +0100

    SOC documentation updated

--- run/soc/wican/wican-soc.sh
+++ run/soc/wican/wican-soc.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 MQTT_SERVER=192.168.1.1
-SUB="mosquitto_sub -h $MQTT_SERVER -t wican/5432048f421d/can/rx -C 8"
-PUB="mosquitto_pub -h $MQTT_SERVER -t wican/5432048f421d/soc"
+SUB="mosquitto_sub -h $MQTT_SERVER -t wican/12345678901d/can/rx -C 8"
+PUB="mosquitto_pub -h $MQTT_SERVER -t wican/12345678901d/soc"
 CAN_MESSAGE_FILE=/tmp/can-message.txt
+
+# The status of the Li-Ion Battery Controller (LBC) of the Nissan Leaf ZE1 is contained in 8 CAN messages:
+#
+# {"bus":"0","type":"rx","ts":42623,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[16,53,97,1,0,0,0,0]}]}
+# {"bus":"0","type":"rx","ts":42735,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[33,2,175,0,0,2,189,255]}]}
+# {"bus":"0","type":"rx","ts":42743,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[34,255,252,24,23,30,48,212]}]}
+# {"bus":"0","type":"rx","ts":42755,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[35,135,8,56,44,3,143,0]}]}
+# {"bus":"0","type":"rx","ts":42764,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[36,1,169,198,36,47,0,5]}]}
+# {"bus":"0","type":"rx","ts":42775,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[37,9,224,0,15,179,52,128]}]}
+# {"bus":"0","type":"rx","ts":42783,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[38,0,5,0,0,2,189,0]}]}
+# {"bus":"0","type":"rx","ts":42795,"frame":[{"id":1979,"dlc":8,"rtr":false,"extd":false,"data":[39,0,3,171,1,174,255,255]}]}
 
 while true; do
   echo "Waiting for messages ..."
--- run/soc/wican/wican-status.sh
+++ run/soc/wican/wican-status.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 MQTT_SERVER=192.168.1.1
-SUB="mosquitto_sub -h $MQTT_SERVER -t wican/5432048f421d/status -C 1"
-PUB="mosquitto_pub -h $MQTT_SERVER -t wican/5432048f421d/can/tx"
+SUB="mosquitto_sub -h $MQTT_SERVER -t wican/12345678901d/status -C 1"
+PUB="mosquitto_pub -h $MQTT_SERVER -t wican/12345678901d/can/tx"
 CAN_STATUS_FILE=/tmp/can-status.txt
 
+# The Li-ion Battery Controller (LBC) of the Nissan Leaf ZE1 accepts the multi-message query only.
+# In order to request the status the following 2 CAN messages have to be sent with a small delay of 100ms.
 requestSoc() {
 echo "Requesting SOC ..."
 $PUB -m "{\"bus\":\"0\",\"type\":\"tx\",\"frame\":[{\"id\":1947,\"dlc\":8,\"rtr\":false,\"extd\":false,\"data\":[2,33,1,0,0,0,0,0]}]}"
